a4d3087858f781ac3429601839163f8cbb602ca4
Devan <devanramadhan92@gmail.com>
2020-12-12 12:58:10 +0700

    25

--- application/modules/master/views/layout/footer.php
+++ application/modules/master/views/layout/footer.php
@@ -1,26 +1,26 @@
     <!-- Main JS -->
-    <script src="<?= base_url() ?>assets/js/vendor.min.js"></script>
+    <script src="<?= base_url() ?>asset/js/vendor.min.js"></script>
 
     <!-- Plugin JS -->
-    <script src="<?= base_url() ?>assets/libs/moment/moment.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/apexcharts/apexcharts.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/flatpickr/flatpickr.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/datatables/dataTables.bootstrap4.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/datatables/dataTables.responsive.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/datatables/responsive.bootstrap4.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/multiselect/jquery.multi-select.js"></script>
-    <script src="<?= base_url() ?>assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/magnific-popup/jquery.magnific-popup.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/select2/select2.min.js"></script>
-    <script src="<?= base_url() ?>assets/libs/croppie/croppie.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/moment/moment.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/flatpickr/flatpickr.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/datatables/dataTables.responsive.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/datatables/responsive.bootstrap4.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/bootstrap-tagsinput/bootstrap-tagsinput.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/multiselect/jquery.multi-select.js"></script>
+    <script src="<?= base_url() ?>asset/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/select2/select2.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/croppie/croppie.min.js"></script>
 
-    <script src="<?= base_url() ?>assets/libs/sweetalert2/dist/sweetalert2.min.js"></script>
-    <script src="<?= base_url() ?>assets/js/app.min.js"></script>
+    <script src="<?= base_url() ?>asset/libs/sweetalert2/dist/sweetalert2.min.js"></script>
+    <script src="<?= base_url() ?>asset/js/app.min.js"></script>
 
     <!-- Custom JS -->
-    <script src="<?= base_url() ?>assets/custom/custom.js"></script>
+    <script src="<?= base_url() ?>asset/custom/custom.js"></script>
 
     <script>
         // Alert
