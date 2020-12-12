151bcd4dff29a5aee87176ad8547ed8361b9195b
Devan <devanramadhan92@gmail.com>
2020-12-12 12:55:20 +0700

    24

--- application/modules/master/views/layout/header.php
+++ application/modules/master/views/layout/header.php
@@ -10,26 +10,26 @@
 	<link rel="shortcut icon" href="<?= $core['mini_logo'] ?>">
 
 	<!-- Plugin CSS -->
-	<link href="<?= base_url() ?>asset/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/bootstrap-tagsinput/bootstrap-tagsinput.css" rel="stylesheet" />
-	<link href="<?= base_url() ?>asset/libs/select2/select2.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/multiselect/multi-select.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/croppie/croppie.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/sweetalert2/dist/sweetalert2.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/placeholder-loading/placeholder-loading.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/bootstrap-tagsinput/bootstrap-tagsinput.css" rel="stylesheet" />
+	<link href="<?= base_url() ?>assets/libs/select2/select2.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/multiselect/multi-select.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/croppie/croppie.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/sweetalert2/dist/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/placeholder-loading/placeholder-loading.min.css" rel="stylesheet" type="text/css" />
 
 	<!-- Main CSS -->
-	<link href="<?= base_url() ?>asset/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/css/icons.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/css/app.min.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url() ?>asset/libs/font-awesome/css/all.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/css/app.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/libs/font-awesome/css/all.css" rel="stylesheet" type="text/css" />
 
 	<!-- Custom CSS -->
-	<link href="<?= base_url() ?>asset/custom/custom.css" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url() ?>assets/custom/custom.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
